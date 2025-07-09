6cfb4974209b1e67d1045a46e75748dd22972fc8
David <davidromashkov@gmail.com>
2025-07-09 09:03:26 +0200

    feat: /help and /track

--- internal/dispatcher/handlers.go
+++ internal/dispatcher/handlers.go
@@ -13,12 +13,24 @@ handlers - исполнители (обработчики команд)
 (например, "Привет, я бот" или "добавить активность")
 */
 func HandleStart(bot interfaces.StoppableBot, msg *tgbotapi.Message) {
+	//We get the chat ID to know who to send the reply to.
 	chatID := msg.Chat.ID
-	text := "Finaly"
-
-	message := tgbotapi.NewMessage(chatID, text)
-	_, err := bot.Send(message)
+	//creating button text
+	buttonTrack := tgbotapi.NewKeyboardButton("/track")//a visual button in the keyboard.
+	buttonHelp := tgbotapi.NewKeyboardButton("/help")
+	//merge two button in row for display ux
+	row := tgbotapi.NewKeyboardButtonRow(buttonTrack, buttonHelp)
+	//creating object keyboard
+	keyboard := tgbotapi.NewReplyKeyboard(row)
+	// creating message text
+	text := "Choose a action"//gets the user 
+	message := tgbotapi.NewMessage(chatID, text)//chatID-to whom to send,text-What to say
+	//attach the keyboard
+	message.ReplyMarkup = keyboard//"Show this keyboard along with the message."
+	//send
+	_, err := bot.Send(message)//Sends any text, emoji, command, button
 	if err != nil {
-		log.Error().Err(err).Msg("Error when sending the /start command")
+		log.Error().Err(err).Msg("Error when sending the /start")
 	}
+
 }
